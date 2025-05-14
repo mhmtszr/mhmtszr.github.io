77abc03fe67ea08ed520b9c25fb8f0f75430bda1
mhmtszr <m.szr98@gmail.com>
2025-05-14 11:46:37 +0300

    Update sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -29,6 +29,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.8,
     },
     {
+      url: 'https://msezer.dev/projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.8,
+    },
+    {
+      url: 'https://msezer.dev/experience',
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as const,
+      priority: 0.8,
+    },
+    {
       url: 'https://msezer.dev/speaking',
       lastModified: new Date(),
       changeFrequency: 'monthly' as const,
